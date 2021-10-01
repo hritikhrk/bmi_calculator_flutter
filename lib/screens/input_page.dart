71db33b5f1f8e995574ea12a9628ab0fe2ee1dc2
Hritik Kumar <hritik.kmr.111@gmail.com>
2021-10-01 14:15:25 +0530

    restructure files and folders

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -1,9 +1,9 @@
-import 'package:bmi_calculator_flutter/results_page.dart';
+import 'package:bmi_calculator_flutter/screens/results_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'icon_content.dart';
-import 'reusable_card.dart';
-import 'constants.dart';
+import 'package:bmi_calculator_flutter/components/icon_content.dart';
+import 'package:bmi_calculator_flutter/components/reusable_card.dart';
+import 'package:bmi_calculator_flutter/constants.dart';
 
 enum Gender { male, female }
 
